9525947ef2211a20da7a6b517b5683adc8ec1f3f
Leondre Darrell Russell <leondrerussell@gmail.com>
2021-10-24 21:44:58 -0500

    Update settings.py

--- bckend_CV/bckend_CV/settings.py
+++ bckend_CV/bckend_CV/settings.py
@@ -19,8 +19,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-q93hzv35a+qcvz(i%qmztuldeu8)w8fs19jrka_xn!hzl7*+28'
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
